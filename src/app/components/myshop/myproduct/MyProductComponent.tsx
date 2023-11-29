f07882c094de8d23d05e3f49ce70b05bcee3df55
Hardin <muhardin@gmail.com>
2023-11-30 06:21:52 +0700

    a

--- src/app/components/myshop/myproduct/MyProductComponent.tsx
+++ src/app/components/myshop/myproduct/MyProductComponent.tsx
@@ -48,7 +48,7 @@ const MyProductComponent = () => {
   });
   /** end of category */
   console.log(productsData);
-  console.log(shopData);
+  console.log("Shop Data : - ", shopData);
   return (
     <div className="flex flex-col gap-2 bg-white pb-20 md:pb-0 p-4 md:p-6">
       <div
