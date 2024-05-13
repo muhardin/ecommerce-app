d881797291027b9248225724fc45179d8a1487be
Hardin <muhardin@gmail.com>
2024-05-13 09:52:55 +0700

    ww

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { getToken } from "next-auth/jwt";
-import { getServerSession } from "next-auth/next";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { options } from "./app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
