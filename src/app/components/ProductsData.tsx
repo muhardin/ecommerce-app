a099ef753a659f000229766e050d4a5d2b4e4ee8
Hardin <muhardin@gmail.com>
2024-07-28 20:00:35 +0700

    ww

--- src/app/components/ProductsData.tsx
+++ src/app/components/ProductsData.tsx
@@ -43,7 +43,7 @@ const ProductsData = ({ item }: { item: ShopProduct }) => {
                 image: `${process.env.SERVER_ENDPOINT}${item.product?.product_gallery[0].url}`,
               },
             }}>
-            <div className=" w-full h-64 group overflow-hidden relative">
+            <div className=" w-full h-80 group overflow-hidden relative">
               {/* <Slider {...settings}>
                 {item.product?.product_gallery.map(
                   (gallery: ProductGallery) => (
@@ -88,7 +88,7 @@ const ProductsData = ({ item }: { item: ShopProduct }) => {
                 image: `/images/no_image.png`,
               },
             }}>
-            <div className=" w-full h-64 group overflow-hidden relative">
+            <div className=" w-full h-80 group overflow-hidden relative">
               <Image
                 src={`/images/no_image.png`}
                 alt="Product image"
