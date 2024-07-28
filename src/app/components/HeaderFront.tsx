5a4e3d9121347e36e2475575820393a87199a158
Hardin <muhardin@gmail.com>
2024-07-28 21:51:46 +0700

    ww

--- src/app/components/HeaderFront.tsx
+++ src/app/components/HeaderFront.tsx
@@ -302,7 +302,7 @@ const HeaderFront = () => {
                   </svg>
                 </span>
               </div> */}
-              <div className="block mr-4 md:hidden">
+              {/* <div className="block mr-4 md:hidden">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -312,7 +312,7 @@ const HeaderFront = () => {
                   viewBox="0 0 16 16">
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                 </svg>
-              </div>
+              </div> */}
               <div className="relative mr-4 ">
                 <span>
                   <div className="absolute top-0 right-0 w-2 h-2 bg-red-400 rounded-full"></div>
